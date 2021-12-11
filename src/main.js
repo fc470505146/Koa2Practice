866bcabf20c7c76156df2283b21b3cc9fe7e88fd
Fu C <2734927248@qq.com>
2021-12-11 15:40:20 +0800

    项目基本优化

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const Koa=require('koa')
+const {APP_PORT} = require('./config/config.default')
+
+const app=new Koa()
+
+app.use(async(ctx,next)=>{
+    ctx.body='hello world'
+})
+
+app.listen(APP_PORT,()=>{
+    console.log(`server is running on
+     http://localhost:${APP_PORT}`);
+})
+
+
