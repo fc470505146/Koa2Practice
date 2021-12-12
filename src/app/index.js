b70b956aae19bb4c243dd0f0536f2669c01a1f86
Fu C <2734927248@qq.com>
2021-12-12 11:40:57 +0800

    尝试部署vue的静态资源

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,14 @@
 const Koa=require('koa')
 const app=new Koa()
-const koaBody=require('koa-body')
+const path=require('path')
+const koa_body=require('koa-body')
+const serve=require('koa-static')
 const userRouter=require('../router/user.route')
-app.use(koaBody())
+
+app.use(serve(path.resolve('public')))
+
+console.log(path.resolve('public'));
+app.use(koa_body())
 app.use(userRouter.routes())
 
 module.exports=app
